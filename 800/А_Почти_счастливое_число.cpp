b73c462b313f641b30ccb65f77dc4df1689342cf
leongit78 <leonidivanov993@gmail.com>
2025-05-23 15:44:48 +0300

    new tasks added

---
+++ 800/А_Почти_счастливое_число.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n;
+    int cnt = 0;
+    cin >> n;
+        
+    while(n > 0)
+    {
+        if(n % 10 == 4 || n % 10 == 7)
+        {
+            cnt++;
+        }
+        n /= 10;
+    }
+    
+    if(cnt == 4 || cnt == 7)
+    {
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+    return 0;
+}
