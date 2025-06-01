d4df166ac8784be2f8180a0584dec2acc7524703
leongit78 <leonidivanov993@gmail.com>
2025-06-02 00:50:42 +0300

    easy  problems

--- 800/А_Панграмма.cpp
+++ 800/А_Панграмма.cpp
@@ -1,8 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 int main()
 {
-    //TODO 
+    int n; cin >> n;
+
+    string s; cin >> s;
+    set<char> unique_symbols;
+
+    for (int i = 0; i < n; ++i)
+    {
+        s[i] = tolower(s[i]);
+        unique_symbols.insert(s[i]);
+    }
+
+    if(unique_symbols.size() < 26 || n < 26)
+        cout << "NO";
+    else
+        cout << "YES"; 
+
     return 0;
 }
