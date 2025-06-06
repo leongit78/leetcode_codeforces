56403944ac77ba92bc3269ed208ad683855d7676
leongit78 <leonidivanov993@gmail.com>
2025-06-07 01:53:08 +0300

    todo

---
+++ medium/В_Режь_чтобы_выжить.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int ll
+#define nl '\n'
+#define sp ' '
+#define pb push_back
+#define IOS ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+
+void solve()
+{
+    IOS;
+    int n,m,a,b; cin >> n >> m >> a >> b;
+    int cnt = 0;
+    //TODO
+}
+
+signed main()
+{
+    int t; cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
